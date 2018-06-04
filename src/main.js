ac4209e5193d3955cfff303508f7bf357f497267
Palash Mondal <palashm@live.in>
2018-06-04 21:23:13 +0530

    Setting the Project up

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import App from './App';
+import router from './router';
+
+Vue.use(Vuetify);
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+});
