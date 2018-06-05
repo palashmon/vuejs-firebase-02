f918a221b0126a08f51bb423bbc2af3af8d2276c
Palash Mondal <palashm@live.in>
2018-06-05 19:38:40 +0530

    Finished menu, added routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,45 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Home from '@/components/Home';
+import Meetups from '@/components/Meetup/Meetups';
+import CreateMeetup from '@/components/Meetup/CreateMeetup';
+import Profile from '@/components/User/Profile';
+import Signup from '@/components/User/Signup';
+import Signin from '@/components/User/Signin';
 
 Vue.use(Router);
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/meetups',
+      name: 'Meetups',
+      component: Meetups
+    },
+    {
+      path: '/meetup/new',
+      name: 'CreateMeetup',
+      component: CreateMeetup
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
+    },
+    {
+      path: '/signin',
+      name: 'Signin',
+      component: Signin
     }
-  ]
+  ],
+  mode: 'history'
 });
