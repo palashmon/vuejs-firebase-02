3d3b03a00a9f5e0c9fd1454efff1c0c4a5c1fe96
Palash Mondal <palashm@live.in>
2018-06-27 20:24:29 +0530

    Added date and time pickers

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -56,6 +56,62 @@
           </v-layout>
           <v-layout row>
             <v-flex xs12 sm6 offset-sm3>
+              <h4>Choose a Data & Time</h4>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="mb-2">
+            <v-flex xs12 sm6 offset-sm3>
+              <v-menu
+                ref="menu2"
+                :close-on-content-click="false"
+                v-model="menu2"
+                :nudge-right="40"
+                :return-value.sync="date"
+                lazy
+                transition="scale-transition"
+                offset-y
+                full-width
+                min-width="290px"
+              >
+                <v-text-field
+                  slot="activator"
+                  v-model="date"
+                  label="Pick a date for event"
+                  prepend-icon="event"
+                  readonly
+                ></v-text-field>
+                <v-date-picker v-model="date" @input="$refs.menu2.save(date)"></v-date-picker>
+              </v-menu>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-menu
+                ref="menu"
+                :close-on-content-click="false"
+                v-model="menu3"
+                :nudge-right="40"
+                :return-value.sync="time"
+                lazy
+                transition="scale-transition"
+                offset-y
+                full-width
+                max-width="290px"
+                min-width="290px"
+              >
+                <v-text-field
+                  slot="activator"
+                  v-model="time"
+                  label="Picker in menu"
+                  prepend-icon="access_time"
+                  readonly
+                ></v-text-field>
+                <v-time-picker v-model="time" @change="$refs.menu.save(time)" format="24hr"></v-time-picker>
+              </v-menu>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
               <v-btn
                 class="primary"
                 :disabled="!formIsValid"
@@ -75,8 +131,15 @@
         title: '',
         location: '',
         imageUrl: '',
-        description: ''
+        description: '',
+        date: null,
+        time: '10:00',
+        menu2: false,
+        menu3: false
       };
+    },
+    beforeMount() {
+      this.date = this.formatDate(new Date());
     },
     computed: {
       formIsValid () {
@@ -84,6 +147,19 @@
           this.location !== '' &&
           this.imageUrl !== '' &&
           this.description !== '';
+      },
+      submittableDateTime () {
+        const date = new Date(this.date);
+        if (typeof this.time === 'string') {
+          let hours = this.time.match(/^(\d+)/)[1];
+          const minutes = this.time.match(/:(\d+)/)[1];
+          date.setHours(hours);
+          date.setMinutes(minutes);
+        } else {
+          date.setHours(this.time.getHours());
+          date.setMinutes(this.time.getMinutes());
+        }
+        return date;
       }
     },
     methods: {
@@ -96,10 +172,18 @@
           location: this.location,
           imageUrl: this.imageUrl,
           description: this.description,
-          date: new Date()
+          date: this.submittableDateTime
         };
         this.$store.dispatch('createMeetup', meetupData);
         this.$router.push('/meetups');
+      },
+      formatDate (date) {
+        if (!date) return null;
+        let d = new Date(date),
+          month = '' + (d.getMonth() + 1),
+          day = '' + d.getDate(),
+          year = d.getFullYear();
+        return [year, month.padStart(2, '0'), day.padStart(2, '0')].join('-');
       }
     }
   };
--- src/store/index.js
+++ src/store/index.js
@@ -10,19 +10,25 @@ export const store = new Vuex.Store({
         imageUrl: 'https://bit.ly/1luHTBM',
         id: 'afajfjadfaadfa323',
         title: 'Meetup in New York',
-        date: '2018-06-26'
+        date: '2018-06-26',
+        location: 'New York',
+        description: 'New York, New York!'
       },
       {
         imageUrl: 'https://bit.ly/1NZ5FP2',
         id: 'aadsfhbkhlk1241',
         title: 'Meetup in Paris',
-        date: '2018-06-25'
+        date: '2018-06-25',
+        location: 'Paris',
+        description: 'It\'s Paris!'
       },
       {
         imageUrl: 'https://bit.ly/2Kpfdty',
         id: 'aadsfhbkhlk1242',
         title: 'Meetup in Vienna',
-        date: '2018-06-27'
+        date: '2018-06-27',
+        location: 'Vienna',
+        description: 'It\'s Vienna, yesss!'
       }
     ],
     user: {
