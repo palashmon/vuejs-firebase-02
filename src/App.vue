1d9f7bd331a768331d630b92c41cb4a35cc1e1cd
Palash Mondal <palashm@live.in>
2018-08-16 20:46:19 +0530

    Prevent unauthenticated Access

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,14 @@
           </v-list-tile-action>
           <v-list-tile-content>{{ item.title }}</v-list-tile-content>
         </v-list-tile>
+        <v-list-tile
+          v-if="userIsAuthenticated"
+          @click="onLogout">
+          <v-list-tile-action>
+            <v-icon>exit_to_app</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>Logout</v-list-tile-content>
+        </v-list-tile>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar dark class="primary">
@@ -29,6 +37,13 @@
           :to="item.link">
           <v-icon left dark>{{ item.icon }}</v-icon>
           {{ item.title }}
+        </v-btn>
+        <v-btn
+          v-if="userIsAuthenticated"
+          flat
+          @click="onLogout">
+          <v-icon left dark>exit_to_app</v-icon>
+          Logout
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
@@ -67,7 +82,11 @@ export default {
       return !!this.$store.getters.user;
     }
   },
-  name: 'App'
+  methods: {
+    onLogout () {
+      this.$store.dispatch('logout');
+    }
+  }
 };
 </script>
 <style>
